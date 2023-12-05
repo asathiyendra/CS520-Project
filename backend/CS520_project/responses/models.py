39ad874947eeb7eb1eacac03d03fd8eff36b7243
Rishik Janaswamy <rishikjanaswamy@vl965-172-31-211-118.wireless.umass.edu>
2023-12-05 10:20:01 -0500

    Created endpoints for User's responses (adding to the databse)

--- backend/CS520_project/responses/models.py
+++ backend/CS520_project/responses/models.py
@@ -1,12 +1,34 @@
 from django.db import models
+from datetime import date
+from prompts.models import Prompt
 
+    
+class Users(models.Model):
+    userid = models.AutoField(primary_key=True, unique=True, default=0)
+    username = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+    
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        db_table = 'users'
+    
 class Responses(models.Model):
+    responseid = models.AutoField(primary_key=True, unique=True)
+    userid = models.ForeignKey(Users, on_delete=models.CASCADE, null=True, db_column='userid')
+    promptid = models.ForeignKey(Prompt, on_delete=models.CASCADE, null=True, db_column='promptid')
     text = models.TextField()
-    date = models.DateField()
-    likes = models.IntegerField()
+    visibility = models.TextField(default="private")
+    date = models.DateField(auto_now_add=True)
+    # likes = models.IntegerField()
     
     def __str__(self):
         return self.text
+    
+    class Meta:
+        db_table = 'responses'
 
 class Comments(models.Model):
     text = models.TextField()
--- backend/CS520_project/responses/views.py
+++ backend/CS520_project/responses/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Responses # replace with your model
+from .serializers import ResponseSerializer  # replace with your serializer
+
+@api_view(['POST'])
+def store_response(request):
+    serializer = ResponseSerializer(data=request.data)
+    print(request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
